4cde7eeb0c2d6dc3bd9d29dad483e9ce725a071c
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-01-18 21:30:18 +0000

    connection to db

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,6 @@
 const { Pool } = require('pg');
 
-const ENV = process.env.NODE_ENV || 'development';
-
-const config = {};
-
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
+const ENV = process.env.NODE_ENV || 'development' || 'production'
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -16,6 +9,14 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
+
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
 
 
 
